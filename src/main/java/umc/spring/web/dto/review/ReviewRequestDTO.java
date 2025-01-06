cfa5aeb2db2e6f9520672c6f88ca03db647a0102
SongMinhyuk <dnfldpden32@gmail.com>
2025-01-06 16:09:32 +0900

    feat: 리뷰 작성기능 요청시 validation추가

--- src/main/java/umc/spring/web/dto/review/ReviewRequestDTO.java
+++ src/main/java/umc/spring/web/dto/review/ReviewRequestDTO.java
@@ -1,10 +1,18 @@
 package umc.spring.web.dto.review;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record ReviewRequestDTO() {
     //TODO: 1. 등록된 멤버인지 확인
     public record RegisterDTO(
+            @NotBlank(message = "멤버 ID는 필수입니다.")
             Long memberId,
+            @NotBlank(message = "리뷰 내용은 필수입니다.")
             String body,
+            @Max(value = 5, message = "리뷰 점수는 5점을 넘을 수 없습니다.")
+            @Min(value = 0, message = "리뷰 점수는 0점 미만일 수 없습니다.")
             Float score
     ){
 
