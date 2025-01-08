2eaa0bd99d98ff9bf496976712f74cb7669da9d6
Miging <32047017+Miging@users.noreply.github.com>
2025-01-08 15:03:19 +0900

    feat: 가게 리뷰 조회기능 구현 (#17)

--- src/main/java/umc/spring/repository/review/ReviewRepository.java
+++ src/main/java/umc/spring/repository/review/ReviewRepository.java
@@ -1,11 +1,15 @@
 package umc.spring.repository.review;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.domain.Review;
+import umc.spring.domain.Store;
 
 @Repository
 @Transactional(readOnly = true)
 public interface ReviewRepository extends JpaRepository<Review,Long>,ReviewRepositoryCustom {
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
 }
