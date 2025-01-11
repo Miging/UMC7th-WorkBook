c2aa85ab82e38b59af439e4e9c8fd86f3e9281dc
SongMinhyuk <dnfldpden32@gmail.com>
2025-01-11 15:17:29 +0900

    feat: Entity, RequestDTO 수정

--- src/main/java/umc/spring/domain/Member.java
+++ src/main/java/umc/spring/domain/Member.java
@@ -1,5 +1,13 @@
 package umc.spring.domain;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,20 +17,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
 import umc.spring.domain.common.BaseEntity;
 import umc.spring.domain.enums.Gender;
 import umc.spring.domain.enums.MemberStatus;
+import umc.spring.domain.enums.Role;
 import umc.spring.domain.enums.SocialType;
 import umc.spring.domain.mapping.MemberAgree;
 import umc.spring.domain.mapping.MemberMission;
@@ -37,56 +40,64 @@ import umc.spring.domain.mapping.MemberPrefer;
 @AllArgsConstructor
 public class Member extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false,length = 20)
-    private String name;
-    @Column(nullable = false,length = 40)
-    private String specAddress;
-    @Column(nullable = false,length = 40)
-    private String address;
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(10)")
-    private Gender gender;
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(10)")
-    private SocialType socialType;
-    @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
-    private MemberStatus status;
-    private LocalDate inactiveDate;
-    @Column(length = 50)
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 20)
+	private String name;
+	@Column(nullable = false, length = 40)
+	private String specAddress;
+	@Column(nullable = false, length = 40)
+	private String address;
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "VARCHAR(10)")
+	private Gender gender;
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "VARCHAR(10)")
+	private SocialType socialType;
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+	private MemberStatus status;
+	private LocalDate inactiveDate;
+	@Column(length = 50)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @ColumnDefault("0")
-    private Integer point;
+	@ColumnDefault("0")
+	private Integer point;
 
-    @OneToMany(mappedBy = "member",cascade = CascadeType.ALL)
-    private List<MemberAgree> memberAgrees=new ArrayList<>();
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+	private List<MemberAgree> memberAgrees = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member",cascade = CascadeType.ALL)
-    private List<MemberMission> memberMissions=new ArrayList<>();
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+	private List<MemberMission> memberMissions = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member",cascade = CascadeType.ALL)
-    private List<MemberPrefer> memberPrefers=new ArrayList<>();
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+	private List<MemberPrefer> memberPrefers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member",cascade = CascadeType.ALL)
-    private List<Review> reviews=new ArrayList<>();
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+	private List<Review> reviews = new ArrayList<>();
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", specAddress='" + specAddress + '\'' +
-                ", address='" + address + '\'' +
-                ", gender=" + gender +
-                ", socialType=" + socialType +
-                ", status=" + status +
-                ", inactiveDate=" + inactiveDate +
-                ", email='" + email + '\'' +
-                ", point=" + point +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Member{"
+			+ "id=" + id
+			+ ", name='" + name + '\''
+			+ ", specAddress='" + specAddress + '\''
+			+ ", address='" + address + '\''
+			+ ", gender=" + gender
+			+ ", socialType=" + socialType
+			+ ", status=" + status
+			+ ", inactiveDate=" + inactiveDate
+			+ ", email='" + email + '\''
+			+ ", point=" + point
+			+ '}';
+	}
+
+	public void encodedPassword(String password) {
+		this.password = password;
+	}
 }
--- src/main/java/umc/spring/web/dto/member/MemberRequestDTO.java
+++ src/main/java/umc/spring/web/dto/member/MemberRequestDTO.java
@@ -1,32 +1,55 @@
 package umc.spring.web.dto.member;
 
+import java.util.Collections;
+import java.util.List;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.List;
+import umc.spring.domain.enums.Role;
 import umc.spring.validation.annotation.ExistCategories;
 
 public record MemberRequestDTO() {
 
-    public record JoinDto(
-            @NotBlank
-            String name,
-            @NotNull
-            Integer gender,
-            @NotNull
-            Integer birthYear,
-            @NotNull
-            Integer birthMonth,
-            @NotNull
-            Integer birthDay,
-            @Size(min = 5,max = 12)
-            String address,
-            @Size(min = 5,max = 12)
-            String specAddress,
-            @ExistCategories
-            List<Long> preferCategory
-    ){
-        // 빈 객체를 나타내는 상수
-        public static final JoinDto EMPTY = new JoinDto("", null, null, null, null, "", "", List.of());
-    }
+	public record JoinDto(
+		@NotBlank
+		String name,
+		@NotBlank
+		@Email
+		String email,
+		@NotBlank
+		String password,
+		@NotNull
+		Integer gender,
+		@NotNull
+		Integer birthYear,
+		@NotNull
+		Integer birthMonth,
+		@NotNull
+		Integer birthDay,
+		@Size(min = 5, max = 12)
+		String address,
+		@Size(min = 5, max = 12)
+		String specAddress,
+		@ExistCategories
+		List<Long> preferCategory,
+		@NotNull
+		Role role
+	) {
+		// 빈 객체를 나타내는 상수
+		public static final JoinDto EMPTY = new JoinDto(
+			"",
+			"",
+			"",
+			0,
+			0,
+			1,
+			1,
+			"",
+			"",
+			Collections.emptyList(),
+			null
+		);
+	}
 }
